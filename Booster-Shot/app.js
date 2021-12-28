47e27985438b55724671026a4773d3e33f2b9815
Akshaya <chanchala9119@gmail.com>
2021-12-29 02:57:45 +0530

    Booster Shot added

---
+++ Booster-Shot/app.js
@@ -0,0 +1,27 @@
+console.log('page attached properly')
+//obtain the target elements from the document
+let btn = document.getElementById('btn')
+
+//button submitting
+btn.addEventListener('click', function (e) {
+    //e.preventDefault()
+    //tapping into the properties
+    //values would only be obtained once the submit button is clicked
+    let fname = document.getElementById('fname').value
+    let lname = document.getElementById('lname').value
+    let age = document.getElementById('age').value
+    let jobType = document.getElementById('dropdown').value
+
+    document.querySelector('.hellomsg').textContent = "Hello " + fname + " !"
+    //convert age to number
+    age = Number(age)
+    if (age >= 60) {
+        document.querySelector('.msg').textContent = "You are eligible for a booster shot!"
+    }
+    else if (jobType === "frontline worker" || jobType === "healthcare worker") {
+        document.querySelector('.msg').textContent = "You are eligible for a booster shot!"
+    }
+    else {
+        document.querySelector('.msg').textContent = "You are not eligible for a booster shot!"
+    }
+})
---
+++ Booster-Shot/ex.js
@@ -0,0 +1,7 @@
+let str = "xxyyzzzz"
+function balanced(str){
+    //count the number of x's y's and z's
+    for(let i=0;i<str.length;i++){
+        
+    }
+}
